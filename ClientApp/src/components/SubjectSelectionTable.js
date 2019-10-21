35c1ffb2a7bde561a4aca606194db96f002fd936
Michal Žáček <michal.zacek@mensagymnazium.cz>
2019-10-21 20:27:40 +0200

    Fixed issues, final version

--- ClientApp/src/components/SubjectSelectionTable.js
+++ ClientApp/src/components/SubjectSelectionTable.js
@@ -6,6 +6,44 @@ import "../style/SubjectSelectionTable.css";
 import { dayNames, periodNames } from "../objects/Period";
 
 export default class SubjectSelectionTable extends Component {
+    renderPeriod(period, periodName, subjects) {
+        return (
+            <div key={period} className="subjectsRow scheduleRow">
+                <div className="periodName">
+                    {periodName}
+                </div>
+        
+                <div className="subjectsSubRow">
+                    {
+                        subjects.map(subject => {
+                            return <SubjectCell subject={subject} bold={subject.id === this.props.selectedSubject} key={subject.id}/>;
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+
+    renderDay(day, dayName, subjects) {
+        return (
+            <React.Fragment key={day}>
+                <div className="dayName scheduleRow">
+                    {dayName}
+                </div>
+
+                {
+                    periodNames.map((periodName, period) => {
+                        var periodSubjects = subjects.filter(subject => subject.period.period === period);
+                        if (periodSubjects.length) {
+                            return this.renderPeriod(period, periodName, periodSubjects);
+                        }
+                        return null;
+                    })
+                }
+            </React.Fragment>
+        );
+    }
+
     render() {
         return (<>
             <div className="subjectTable">
@@ -13,36 +51,13 @@ export default class SubjectSelectionTable extends Component {
                     dayNames.map((dayName, day) => {
                         var daySubjects = this.props.subjects.filter(subject => subject.period.day === day);
                         if (daySubjects.length) {
-                            return (<React.Fragment key={day}>
-                                <div className="dayName scheduleRow">{dayName}</div>
-                                {
-                                    periodNames.map((periodName, period) => {
-                                        var periodSubjects = daySubjects.filter(subject => subject.period.period === period);
-                                        if (periodSubjects.length) {
-                                            return (<div key={period} className="scheduleRow">
-                                                <div className="periodName">
-                                                    {periodName}
-                                                </div>
-                                                
-                                                <div className="subjectsSubRow">
-                                                    {
-                                                        periodSubjects.map(subject => {
-                                                            return <SubjectCell subject={subject} bold={subject.id === this.props.selectedSubject} key={subject.id}/>;
-                                                        })
-                                                    }
-                                                </div>
-                                            </div>);
-                                        }
-                                        return null;
-                                    })
-                                }
-                            </React.Fragment>);
+                            return this.renderDay(day, dayName, daySubjects);
                         }
                         return null;
                     })
                 }
             </div>
-        </>)
+        </>);
     }
 }
 
