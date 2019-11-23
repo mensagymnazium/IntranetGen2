ed2d7aed0d2c4f2944817a7f701909fb28271771
Martin-Kent Kraus <Martin_Kent.Kraus@mensagymnazium.cz>
2019-11-23 12:40:18 +0100

    Added simple error messages, so that fields now require to be filled in

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -11,6 +11,7 @@ export default class Login extends Component {
             error: "",
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(event) {
@@ -18,6 +19,19 @@ export default class Login extends Component {
             [event.target.name]: event.target.value
         })
     }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.state.username == "") {
+            return this.setState({ error: "Zadejte uživatelské jméno" });
+        }
+        else if (this.state.password == "") {
+            return this.setState({ error: "Zadejte heslo" });
+        }
+        else {this.setState({ error: "" })}
+    }
+
+    
   render() {
     return (<>
       <Helmet>
@@ -27,10 +41,21 @@ export default class Login extends Component {
       <h1>
         Přihlašte se
       </h1>
-        <form>
-            <input type="text" name="username" placeholder="Uživatelské jméno"/> 
+
+        
+
+            
+
+        <form onSubmit={this.handleSubmit}>
+            {
+                
+                <h3>
+                    {this.state.error}
+                </h3>
+            }
+            <input type="text" name="username" placeholder="Uživatelské jméno" onChange = { this.handleChange } /> 
             <br />
-            <input type="text" name="password" placeholder="Heslo" /> 
+            <input type="text" name="password" placeholder="Heslo" onChange={this.handleChange}/> 
             <br/>
             <input type="submit" value="Přihlásit" />
             
--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+ 
 export default class Register extends Component {
     constructor() {
         super();
@@ -9,6 +10,7 @@ export default class Register extends Component {
             lastname: "",
             email: "",
             password: "",
+            grade: "",
             error: "",
         }
         this.handleChange = this.handleChange.bind(this)
@@ -20,9 +22,29 @@ export default class Register extends Component {
             [event.target.name]: event.target.value
         })
     }
+
     handleSave(event) {
         event.preventDefault();
         const userinfo = new FormData(event.target);
+        if (this.state.firstname == "") {
+            return this.setState({ error: "Zadejte jméno" });
+        }
+        else if (this.state.lastname == "") {
+              return this.setState({ error: "Zadejte příjmení" });
+          }
+        else if (this.state.username == "") {
+            return this.setState({ error: "Zadejte uživatelské jméno" });
+        }
+        else if (this.state.password == "") {
+            return this.setState({ error: "Zadejte heslo" });
+        }
+        else if (this.state.email == "") {
+            return this.setState({ error: "Zadejte email" });
+        }
+        else { this.setState({ error: "" }) }
+        
+       
+
         fetch("api/StudentDb", { method: "Post", body: userinfo });
         }
 
@@ -35,13 +57,16 @@ export default class Register extends Component {
            
 
 
-            <form>
+            <form onSubmit={this.handleSave}>
+                <h3>
+                    {this.state.error}
+                </h3>
                 <label>Jméno</label>
-                <input type="text" name="firstname" />
+                <input type="text" name="firstname" onChange={this.handleChange} />
                 <label>Přijmení</label>
-                <input type="text" name="lastname" />
+                <input type="text" name="lastname" onChange={this.handleChange}/>
                 <label>Třída</label>
-                <select>
+                <select name="grade" onChange={this.handleChange}>
                     <option value="Prima">Prima</option>
                     <option value="Sekunda">Sekunda</option>
                     <option value="Tercie">Tercie</option>
@@ -53,14 +78,14 @@ export default class Register extends Component {
                 </select> 
                 <br />
                 <label>Uživatelské jméno</label>
-                <input type="text" name="username" />
-                <br />
-                <label>Mail</label>
-                <input type="text" name="email" />             
+                <input type="text" name="username" onChange={this.handleChange} />
                 <br />
                 <label>Heslo</label>
-                <input type="text" name="password"/>
-
+                <input type="text" name="password" onChange={this.handleChange} />
+                <br />
+                <label>Mail</label>
+                <input type="text" name="email" onChange={this.handleChange}/>             
+                <br />
                 <br />
                 <input type="submit" value="Registrovat" />
             </form>
