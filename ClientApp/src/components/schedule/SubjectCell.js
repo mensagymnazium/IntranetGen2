dcd57371f085c1127ed7d4f9081f90945ece40be
Benjamin Swart <Benjaminswart@email.cz>
2019-12-23 13:35:38 +0100

    Fixed subject deletion

--- ClientApp/src/components/schedule/SubjectCell.js
+++ ClientApp/src/components/schedule/SubjectCell.js
@@ -25,7 +25,7 @@ export default class SubjectCell extends Component {
         {this.props.editMode &&
           <div className="card-footer"><div className="btn-group">
             <button className="btn btn-primary subjectCellEdit subjectCellCRUD" onClick={() => this.props.edit(this.props.subject.id)}><img height="30vh" src={Icons.edit} alt="Edit Icon" /></button>
-            <button className="btn btn-danger subjectCellRemove subjectCellCRUD" onClick={() => this.props.remove(this.props.subject.id)}><img height="30vh" src={Icons.delete} alt="Remove Icon" /></button>
+            <button className="btn btn-danger subjectCellRemove subjectCellCRUD" onClick={() => this.props.delete(this.props.subject.id)}><img height="30vh" src={Icons.delete} alt="Remove Icon" /></button>
           </div></div>
         }
 
