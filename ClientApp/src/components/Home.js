5f6d8a3a7beff647dff4d1564b0050bfb78b0858
Stany <stany.caja@gmail.com>
2020-03-16 19:40:31 +0100

    Change of home page text

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -36,21 +36,8 @@ export const Home = props => {
       </Helmet>
       <div className="container">
         <div className="alert alert-warning border border-warning">
-          <p>
-            Toto je hlavní stránka nového Intranetu, kterou v současné době
-            vyvíjí <b>Benjamin Swart</b>, <b>Petr Dedek</b>,{" "}
-            <b>Martin-Kent Kraus</b> a <b>Michal Žáček</b> pod vedením{" "}
-            <b>Stanislava Čaji</b>.
-          </p>
-          <p>
-            Pokud jste se sem dostali a nejste součástí tohoto týmu, což nevím,
-            jestli je vůbec možné, tak vás ujišťuji, že se nejedná o nic
-            důležitého a že tu zatím žádné předměty přihlásit nejdou.
-          </p>
-          <p>
-            Pokud jste součástí tohoto týmu, tak vás prosím, aby jste psali
-            čitelný kód.
-          </p>
+          <p>Toto je hlavní stránka nového Intranetu</p>
+          <p>Ještě není jasný, jestli sem něco dáme nebo ne</p>
           <p>
             Tato stránka byla vykreslena v <b>{new Date().toLocaleString()}</b>.
           </p>
